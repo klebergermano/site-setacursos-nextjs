b8a2e0ea75481c9dce12e5b8ecb379e49b4e7bf2
Kleber Germano <klebergermanodev@gmail.com>
2026-05-25 17:23:51 -0300

    Verificação Google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,25 @@
 
 
 import Script from "next/script";
+export const metadata = {
+    title: "Seta Cursos",
+    description: "Cursos de T.I, Programação, Informática, Excel, Inglês e Tecnologia com foco prático para o mercado de trabalho.",
 
+    keywords: [
+        "curso de T.I",
+        "curso de Tecnologia da Informação",
+        "curso de programação",
+        "curso de informática",
+        "curso de excel",
+        "curso de inglês",
+        "seta cursos"
+    ],
+    authors: [{ name: "Seta Cursos" }],
+
+    verification: {
+        google: "ztY3CureYTqEE4UxlOqDUYKKNzAlFRthvBV2Q78OX7k",
+    },
+};
 export default function RootLayout({ children }) {
 
 
